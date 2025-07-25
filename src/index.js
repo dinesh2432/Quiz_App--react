391ad3a35a40b3efa9da69b27ba6d62a462e7423
dinesh2432 <ddk113311@gmail.com>
2025-07-25 21:31:15 +0530

    Remove basename from HashRouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter, HashRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename="/Quiz_App--react">
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode> 
